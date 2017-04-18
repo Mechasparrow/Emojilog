40bebfa7821e0e6838f8f506c09966c5715898ba
System <none>
2017-04-18 00:22:03 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# SQLite3 with Sequelize
+A simple list app that demonstrates creating and interacting with a SQLite3 database using the Sequelize ORM.
+
+![](https://cdn.glitch.com/5dd56de5-79af-444e-a17e-bd60230962eb%2FsqliteDBGIF.gif)
+
+## Getting Started
+Remix and specify a username and password for your database by setting `DB_USER` and `DB_PASS` in `.env`.
---
+++ server.js
@@ -0,0 +1,102 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var Sequelize = require('sequelize');
+var app = express();
+
+// default user list
+var users = [
+      ["John","Hancock"],
+      ["Liz","Smith"],
+      ["Ahmed","Khan"]
+    ];
+var User;
+
+// setup a new database
+// using database credentials set in .env
+var sequelize = new Sequelize('database', process.env.DB_USER, process.env.DB_PASS, {
+  host: '0.0.0.0',
+  dialect: 'sqlite',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+    // Security note: the database is saved to the file `database.sqlite` on the local filesystem. It's deliberately placed in the `.data` directory
+    // which doesn't get copied if someone remixes the project.
+  storage: '.data/database.sqlite'
+});
+
+// authenticate with the database
+sequelize.authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+    // define a new table 'users'
+    User = sequelize.define('users', {
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      }
+    });
+    
+    setup();
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database: ', err);
+  });
+
+// populate table with default users
+function setup(){
+  User.sync({force: true}) // using 'force' it drops the table users if it already exists, and creates a new one
+    .then(function(){
+      // Add the default users to the database
+      for(var i=0; i<users.length; i++){ // loop through all users
+        User.create({ firstName: users[i][0], lastName: users[i][1]}); // create a new entry in the users table
+      }
+    });  
+}
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/users", function (request, response) {
+  var dbUsers=[];
+  User.findAll().then(function(users) { // find all entries in the users tables
+    users.forEach(function(user) {
+      dbUsers.push([user.firstName,user.lastName]); // adds their info to the dbUsers value
+    });
+    response.send(dbUsers); // sends dbUsers back to the page
+  });
+});
+
+// creates a new entry in the users table with the submitted values
+app.post("/users", function (request, response) {
+  User.create({ firstName: request.query.fName, lastName: request.query.lName});
+  response.sendStatus(200);
+});
+
+// drops the table users if it already exists, populates new users table it with just the default users.
+app.get("/reset", function (request, response) {
+  setup();
+  response.redirect("/");
+});
+
+// removes all entries from the users table
+app.get("/clear", function (request, response) {
+  User.destroy({where: {}});
+  response.redirect("/");
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
