839a7b4d9de549e2300eb7abf4f61d4a0130c945
Michael Navazhylau <mikipux7@gmail.com>
2017-04-26 20:57:40 -0500

    Added header page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id = "header-comp">
-    <p>Definietyl here </p>
+  <div>
+    <div id = "header-comp">
+
+    </div>
+    <div id = "header-main">
+      <h2 color = "white">Emojielog</h2>
+    </div>
   </div>
 </template>
 
@@ -9,3 +14,28 @@
     name: 'my-header'
   }
 </script>
+
+<style scoped>
+
+  #header-comp {
+    height: 15px;
+    width: 100%;
+    background-color:#004C8B;
+  }
+
+  #header-main {
+    width: 100%;
+    text-align:center;
+    background-color: #0277BC
+  }
+
+  #header-main h2 {
+    margin: 0px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    color:white;
+  }
+
+
+
+</style>
